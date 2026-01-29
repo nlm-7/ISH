793a593375775140583ca8d679a5661b7150373f
nlm-7 <nilamtak712@gmail.com>
2026-01-30 00:46:40 +0530

    Initial commit

---
+++ src/main/java/com/neelam/ish/IshApplication.java
@@ -0,0 +1,13 @@
+package com.neelam.ish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IshApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neelam/ish/IshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neelam.ish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
